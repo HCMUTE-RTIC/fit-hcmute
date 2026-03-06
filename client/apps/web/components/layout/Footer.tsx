43d3f8046aa39ea6cce567936145d26e22b90ab6
Hồ Việt Anh <138989039+AndyAnh174@users.noreply.github.com>
2026-03-06 10:16:34 +0700

    Update footer text with new club name and link

--- client/apps/web/components/layout/Footer.tsx
+++ client/apps/web/components/layout/Footer.tsx
@@ -112,7 +112,7 @@ export default function Footer() {
             <span style={{ color: 'var(--color-accent-500)' }}>❤</span> Kỷ niệm 25 năm thành lập (2001 - 2026)
           </p>
           <p className="mt-2 text-sm">
-            Được phát triển bởi <span className="font-semibold" style={{ color: 'var(--color-primary-600)' }}>Câu Lạc Bộ RTIC</span> 
+            Được phát triển bởi <span href="https://hcmutertic.com" target="_blank" rel="noopener noreferrer" className="font-semibold" style={{ color: 'var(--color-primary-600)' }}>HCM UTE Research on Technology and Innovation Club</span> 
           </p>
         </div>
       </div>
